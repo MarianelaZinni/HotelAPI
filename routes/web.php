824875757b456b2bbc111e9ab3d7f9676711ac0b
Marianela Zinni <marianela.zinni@gmail.com>
2025-11-27 09:49:26 -0300

    definicion de las rutas para los endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
+use App\Http\Controllers\HotelController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Middleware\ApiKeyMiddleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Exponemos las rutas API desde web.php usando prefijo 'api' y aplicando
+| middleware necesarios.
+|
+*/
+
+Route::prefix('api')->middleware([SubstituteBindings::class, ApiKeyMiddleware::class])->group(function () {
+    // Hotels
+    Route::get('/hotels', [HotelController::class, 'index']);
+    Route::post('/hotels', [HotelController::class, 'store']);
+
+    // Rooms (per hotel)
+    Route::get('/hotels/{hotelId}/rooms', [RoomController::class, 'index']);
+    Route::post('/hotels/{hotelId}/rooms', [RoomController::class, 'store']);
+
+    // Reservations
+    Route::get('/reservations', [ReservationController::class, 'index']); // filtros: from, to, hotel_id, room_id
+    Route::post('/reservations', [ReservationController::class, 'store']);
+    Route::get('/reservations/{id}', [ReservationController::class, 'show']);
 });
